85171e8d34109e4dda8c90921187035cc37b8183
Habiba <habibabassem@aucegypt.edu>
2019-08-18 10:14:16 +0200

    Update global.h

--- GlobalRouter/lefdef/global.h
+++ GlobalRouter/lefdef/global.h
@@ -1,19 +1,5 @@
-#ifndef  _GLOBAL_H_
-#define  _GLOBAL_H_
+#pragma once
 
-#include <stdio.h>
-
-#define  TRUE   1
-#define  FALSE  0
-#define  MAXLONG  0x7fffffffL
-
-struct point
-{
-  long    x, y;
-};
-
-typedef  struct point Point;
-
-typedef  long nn_array[8];
-
-#endif  /* _GLOBAL_H_ */
+const int DEFAULT_POOL_SIZE = 10;
+const int STARTED = 0;
+const int STOPPED = 1;
